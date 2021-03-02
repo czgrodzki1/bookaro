5f9635ebee4de292a1c258a2ef9f842b2575ec9f
Bartosz Czerwonogrodzki <bczerwonogrodzki@atlassian.com>
2021-03-02 08:37:22 +0100

    Adding recipient entity

--- src/main/java/pl/sztukakodu/bookaro/order/domain/Recipient.java
+++ src/main/java/pl/sztukakodu/bookaro/order/domain/Recipient.java
@@ -5,11 +5,19 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Recipient {
+
+    @Id
     private String name;
     private String phone;
     private String street;
