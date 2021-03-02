47efc4ac6cc8f87b91182f420baca8edd2d6053b
Bartosz Czerwonogrodzki <bczerwonogrodzki@atlassian.com>
2021-03-02 08:37:36 +0100

    Adding author entity

---
+++ src/main/java/pl/sztukakodu/bookaro/catalog/domain/Author.java
@@ -0,0 +1,38 @@
+package pl.sztukakodu.bookaro.catalog.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+@Entity
+@NoArgsConstructor
+@ToString(exclude = "books")
+@EntityListeners(AuditingEntityListener.class)
+public class Author {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @ManyToMany(fetch = FetchType.EAGER, mappedBy = "authors")
+    @JsonIgnore
+    private Set<Book> books;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
